2323b141fda69bb250e2ae3b1856039d9c8312ce
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-23 12:16:09 -0500

    add players to arraylist in blackjack app commit

--- src/main/java/com/pluralsight/BlackJackApp.java
+++ src/main/java/com/pluralsight/BlackJackApp.java
@@ -10,11 +10,12 @@ public class BlackJackApp {
         //Player ArrayList
         ArrayList<Player> players = new ArrayList<>();
         players.add(new Player("Mari"));
+        players.add(new Player("Mario"));
+        players.add(new Player("Catrina"));
+        players.add(new Player("Truth"));
 
-
-
-        // deal 5 cards
-        for(int i = 0; i < 5; i++) {
+        // deal 2 cards
+        for(int i = 0; i < 2; i++) {
             // get a card from the deck
             Card card = deck.deal();
             // deal that card to the hand
