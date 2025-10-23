fa6d4799e7512c0c816a4bd7c1aabd982bda1268
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-23 10:59:04 -0500

    start arrayList of players

--- src/main/java/com/pluralsight/BlackJackApp.java
+++ src/main/java/com/pluralsight/BlackJackApp.java
@@ -1,9 +1,16 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class BlackJackApp {
     public static void main(String[] args) {
         Deck deck = new Deck();
         Hand hand1 = new Hand();
+
+        //Player ArrayList
+        ArrayList<Players> player = new ArrayList<>();
+
+        player.
 
         // deal 5 cards
         for(int i = 0; i < 5; i++) {
