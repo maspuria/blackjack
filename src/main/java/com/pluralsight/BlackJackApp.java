2958ea8c0bfcb998f1d9eadbf1ddb7993bae7a63
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-23 10:39:19 -0500

    start blackjack app commit

--- src/main/java/com/pluralsight/BlackJackApp.java
+++ src/main/java/com/pluralsight/BlackJackApp.java
@@ -2,7 +2,20 @@ package com.pluralsight;
 
 public class BlackJackApp {
     public static void main(String[] args) {
+        Deck deck = new Deck();
+        Hand hand1 = new Hand();
 
+        // deal 5 cards
+        for(int i = 0; i < 5; i++) {
+            // get a card from the deck
+            Card card = deck.deal();
+            // deal that card to the hand
+            hand1.deal(card)
+        }
+        int handValue = hand1.getValue();
+        System.out.println("This hand is worth " + handValue);
     }
+
+}
 
 }
